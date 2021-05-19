a521925dc1c40953276d427ca0c405d2540f1084
Jack Schumacher <>
2021-05-19 18:01:22 +0000

    Added Drivers Age Function

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ def personal_name(name):
 def name_len(name):
   length=len(name)
   
-  
+def drivers_age(age):
+  if age >=16:
+    print("You are able to drive")
+  else:
+    print("You are not able to drive")  
 
 def main():
   name = input("What is your name? ")
   age = input("What is your age?")
   personal_name(name)
   name_len(name)
+  drivers_age()
 
 
 main()
