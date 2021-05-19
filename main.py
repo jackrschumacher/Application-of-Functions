0a378d90e13315b4a473530820b729738547fbf5
Jack Schumacher <>
2021-05-19 18:10:42 +0000

    Added Random Number, and tidied up

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from random import randrange
 def main():
   name = input("What is your name? ")
   age = int(input("What is your age?"))
@@ -6,6 +6,7 @@ def main():
   personal_name(name)
   name_len(name)
   drivers_age(age)
+  lucky_number()
 
 
 def personal_name(name):
@@ -13,13 +14,16 @@ def personal_name(name):
 
 def name_len(name):
   length=len(name)
-  
+  print("Your name is",length,"Charachters long")
+
 def drivers_age(age):
   if age >=16:
     print("You are able to drive")
   else:
     print("You are not able to drive")  
 
-
+def lucky_number():
+  number = randrange(1,100,1)
+  print("Your Lucky Number is:",number)
 
 main()
